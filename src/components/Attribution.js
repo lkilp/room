6bf7dd61df99c3ab259c39b90ef221ccc8ed1947
liis.kilp <kilp.liis@gmail.com>
2021-07-05 10:42:04 +0200

    git init and setup

---
+++ src/components/Attribution.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Attribution.scss';
+
+const Attribution = () => {
+    return (
+        <div className="Attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+            Coded by <a href="https://lkilp.github.io/portfolio/" target="_blank">Liis Kilp</a>.
+        </div>
+    )
+}
+export default Attribution;
