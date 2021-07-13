2c28f052c012e54acf9e5e27064f078c23616935
liis.kilp <kilp.liis@gmail.com>
2021-07-13 09:42:26 +0200

    fix routing

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -20,16 +20,16 @@ const Navigation = (props) => {
                 {menuOpen && <div className='mobile-filter' />}
                 {menuOpen && <div className="mobile-menu">
                     <svg className='close' onClick={() => toggleMenu()} width="16" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M14.364.222l1.414 1.414L9.414 8l6.364 6.364-1.414 1.414L8 9.414l-6.364 6.364-1.414-1.414L6.586 8 .222 1.636 1.636.222 8 6.586 14.364.222z" fill="#000" fill-rule="evenodd" opacity=".201" /></svg>
-                    <a href='/'>home</a>
-                    <a href='/'>shop</a>
-                    <a href='/'>about</a>
-                    <a href='/'>contact</a>
+                    <a href='.'>home</a>
+                    <a href='.'>shop</a>
+                    <a href='.'>about</a>
+                    <a href='.'>contact</a>
                 </div>}
                 <img className='logo' src={logo} alt='room logo'></img>
-                <a className='underline' href='/'>home</a>
-                <a className='underline' href='/'>shop</a>
-                <a className='underline' href='/'>about</a>
-                <a className='underline' href='/'>contact</a>
+                <a className='underline' href='.'>home</a>
+                <a className='underline' href='.'>shop</a>
+                <a className='underline' href='.'>about</a>
+                <a className='underline' href='.'>contact</a>
             </nav>
         </div>
     )
